aa75e7ae3f4735f970f3cf48acbd9c4ca463902c
emmy <52672675+emmettgb@users.noreply.github.com>
2023-01-03 16:14:08 -0600

    some pretty cool vec expansions

--- src/Vulta.jl
+++ src/Vulta.jl
@@ -5,6 +5,8 @@ using ToolipsSVG
 
 abstract type VultaVector end
 
+mutable struct
+
 mutable struct Vec2 <: VultaVector
     x::Int64
     y::Int64
@@ -16,11 +18,17 @@ mutable struct Vec3 <: VultaVector
     z::Int64
 end
 
-mutable struct VultaModifier <: ToolipsSession.AbstractComponentModifier
+mutable struct VultaModifier{V <: VultaVector} <: ToolipsSession.AbstractComponentModifier
+    delta::Int64
     rootc::Dict{String, Servable}
+    comps::Dict{String, V}
     changes::Vector{String}
-    function VultaModifier(width::Int64, height::Int64, cm::ComponentModifier)
-        new(cm.rootc, changes)
+    function VultaModifier(delta::Int64, width::Int64, height::Int64, cm::ComponentModifier)
+        new{Vec2}(cm.rootc, comps::Dict{String, Dict{String, V}}, changes)
+    end
+    function VultaModifier(delta::Int64, width::Int64, height::Int64, depth::Int64,
+         cm::ComponentModifier)
+        new{Vec2}(cm.rootc, , changes)
     end
 end
 
@@ -30,21 +38,36 @@ function start(f::Function, c::Connection, width::Int64 = 1280,
         f(vm)
     end
     wind = svg("vulta-main", width = width, height = height)
+    style!(wind, "padding" => 0px, "position" => "absolute")
+    wind::Component{:svg}
 end
 
-function translate!(cm::VultaModifier, comp::Component{<:Any}, v::Vec2)
+function start(f::Function, c::Connection, width::Int64 = 1280,
+    height::Int64 = 720, depth::Int64 = 1000; tickrate::Int64 = 100)
+    throw!("3D has not been implemented")
+    script!(c, "vulta-listener", time = tickrate) do cm::ComponentModifier
+        vm = VultaModifier()
+        f(vm)
+    end
+    wind = svg("vulta-main", width = width, height = height, depth = depth)
+    style!(wind, "padding" => 0px, "position" => "absolute")
+    wind::Component{:svg}
+end
+
+function translate!(cm::VultaModifier{Vec2}, comp::Component{<:Any}, v::Vec2)
 
 end
 
-function is_colliding(vm::VultaModifier, s::Component{<:Any}, s2::Component{<:Any})
+function is_colliding(vm::VultaModifier{Vec2}, s::Component{<:Any}, s2::Component{<:Any})
 
 end
 
-function force!(vm::VultaModifier, s::Component{<:Any}, force::Vec2)
+function force!(vm::VultaModifier{Vec2}, s::Component{<:Any}, force::Vec2)
 
 end
 
-function spawn!(vm::VultaModifier, s::Component{<:Any})
+function spawn!(vm::VultaModifier{Vec2}, s::Component{<:Any},
+    v::Vec2 = Vec2(0, 0))
 
 end
 
