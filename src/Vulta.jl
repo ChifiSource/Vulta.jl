f3e816ca7e1ef0c0ee571df8b881b17178a33438
emmy <52672675+emmettgb@users.noreply.github.com>
2023-01-03 22:32:34 -0600

    Add files via upload

--- src/Vulta.jl
+++ src/Vulta.jl
@@ -21,18 +21,11 @@ mutable struct VultaModifier{V <: VultaVector} <: ToolipsSession.AbstractCompone
     c::Connection
     delta::Int64
     rootc::Dict{String, Servable}
-    comps::Dict{String, V}
     changes::Vector{String}
     function VultaModifier(c::Connection,
         delta::Int64, width::Int64, height::Int64, cm::ComponentModifier)
         comps::Dict{String, Vec2} = Dict{String, Vec2}()
-        for comp in cm.rootc
-            if comp[2].tag ==  "circle"
-                push!(comps, comp[2].name => Vec2(parse(Int64, comp[2]["cx"]),
-                                                parse(Int64, comp[2]["cy"])))
-            end
-        end
-        new{Vec2}(c, delta, cm.rootc, comps, cm.changes)
+        new{Vec2}(c, delta, cm.rootc, cm.changes)
     end
     function VultaModifier(delta::Int64, width::Int64, height::Int64, depth::Int64,
          cm::ComponentModifier)
@@ -50,6 +43,7 @@ function open(f::Function, c::Connection, bod::Component{:body},
         delta += 1
         push!(cm.changes, join(c[:Session].peers[getip(c)][getip(c)]))
         vm = VultaModifier(c, delta, width, height, cm)
+        [force(cm, comp) for comp in values(cm.rootc)]
         f(vm)
         c[:Session].peers[getip(c)][getip(c)] = Vector{String}()
     end
@@ -57,6 +51,72 @@ function open(f::Function, c::Connection, bod::Component{:body},
     push!(bod, wind)
     style!(wind, "padding" => 0px, "position" => "absolute")
     write!(c, bod)
+end
+
+length(c::Component{:circle}) = parse(Int64, c["r"])
+
+length(c::Component{:rect}) = parse(Int64, c["length"])
+
+width(c::Component{:circle}) = parse(Int64, c["r"])
+
+width(c::Component{:rect}) = parse(Int64, c["width"])
+
+function force(cm::ComponentModifier, comp::Component{:circle})
+    if ~("fx" in keys(comp.properties))
+        return
+    end
+    decay = parse(Int64, comp["decay"])
+    currx = parse(Int64, comp["cx"])
+    curry = parse(Int64, comp["cy"])
+    fx = parse(Int64, comp["fx"])
+    fy = parse(Int64, comp["fy"])
+    if fx != 0
+        cm[comp] = "cx" => currx + fx
+        if fx > 0
+            cm[comp] = "fx" => (fx - decay)
+        else
+            cm[comp] = "fx" => fx + (decay)
+        end
+    elseif fy != 0
+        cm[comp] = "cy" => curry + fy
+        if fy > 0
+            cm[comp] = "fy" => (fy - decay)
+        else
+            cm[comp] = "fy" => fy + (decay)
+        end
+    end
+end
+
+function force(cm::ComponentModifier, comp::Component{<:Any})
+    if ~("fx" in keys(comp.properties))
+        return
+    end
+end
+
+function force(cm::ComponentModifier, comp::Component{:rect})
+    if ~("fx" in keys(comp.properties))
+        return
+    end
+    decay = parse(Int64, comp["decay"])
+    currx = parse(Int64, comp["x"])
+    curry = parse(Int64, comp["y"])
+    fx = parse(Int64, comp["fx"])
+    fy = parse(Int64, comp["fy"])
+    if fx != 0
+        cm[comp] = "x" => currx + fx
+        if fx > 0
+            cm[comp] = "fx" => (fx - decay)
+        else
+            cm[comp] = "fx" => fx + (decay)
+        end
+    elseif fy != 0
+        cm[comp] = "y" => curry + fy
+        if fy > 0
+            cm[comp] = "fy" => (fy - decay)
+        else
+            cm[comp] = "fy" => fy + (decay)
+        end
+    end
 end
 
 function rpc!(vm::VultaModifier)
@@ -73,24 +133,43 @@ function close(vm)
 end
 
 function connect(f::Function, c::Connection, bod::Component{:body}, ip::String,
-    width::Int64 = 1280, height::Int64 = 720; tickrate::Int64 = 100)
+    width::Int64 = 1280, height::Int64 = 720; tickrate::Int64 = 20)
 end
 
 function open(f::Function, c::Connection, width::Int64 = 1280,
-    height::Int64 = 720, depth::Int64 = 1000; tickrate::Int64 = 100)
+    height::Int64 = 720, depth::Int64 = 1000; tickrate::Int64 = 20)
     throw!("3D has not been implemented")
 end
 
-function translate!(cm::VultaModifier{Vec2}, comp::Component{<:Any}, v::Vec2)
-
+function translate!(cm::VultaModifier{Vec2}, comp::Component{:circle}, v::Vec2)
+    cm[comp] = "cx" => v.x; cm[comp] = "cy" => v.y
 end
 
-function is_colliding(vm::VultaModifier{Vec2}, s::Component{<:Any}, s2::Component{<:Any})
-
+function translate!(cm::VultaModifier{Vec2}, comp::Component{:rect}, v::Vec2)
+    cm[comp] = "x" => v.x; cm[comp] = "y" => v.y
 end
+
+function is_colliding(vm::VultaModifier{Vec2}, s1::Component{<:Any},
+    s2::Component{<:Any})
+    s1 = vm.rootc[s1.name]
+    s2 = vm.rootc[s2.name]
+    s1pos = position(s1); s2pos = position(s2)
+    diffx, diffy = abs(s1pos.x - s2pos.x), abs(s1pos.y - s2pos.y)
+    if diffy < 5 && diffx < 5
+        true
+    else
+        false
+    end
+end
+
+position(comp::Component{:circle}) = Vec2(parse(Int64, comp["cx"]),
+        parse(Int64, comp["cy"]))
+position(comp::Component{<:Any}) = Vec2(parse(Int64, comp["x"]),
+                parse(Int64, comp["y"]))
 
 function force!(vm::VultaModifier{Vec2}, s::Component{<:Any}, force::Vec2)
-
+    vm[s] = "fx" => force.x
+    vm[s] = "fy" => force.y
 end
 
 function spawn!(vm::VultaModifier{Vec2}, s::Component{<:Any},
@@ -98,8 +177,17 @@ function spawn!(vm::VultaModifier{Vec2}, s::Component{<:Any},
 
 end
 
-function spawn!(vm::VultaModifier{Vec2}, s::Component{:circle}, v::Vec2 = Vec2(0, 0))
-    s["cx"] = v.x; s["cy"] = v.y
+function spawn!(vm::VultaModifier{Vec2}, s::Component{:rect}, v::Vec2 = Vec2(0, 0);
+    decay::Int64 = 1)
+    s["x"] = v.x; s["y"] = v.y; s["fx"] = 0; s["fy"] = 0; s["decay"] = decay
+    style!(s, "transition" => 1seconds)
+    append!(vm, "vulta-main", s)
+end
+
+function spawn!(vm::VultaModifier{Vec2}, s::Component{:circle}, v::Vec2 = Vec2(0, 0);
+    decay::Int64 = 1)
+    s["cx"] = v.x; s["cy"] = v.y; s["fx"] = 0; s["fy"] = 0; s["decay"] = decay
+    style!(s, "transition" => 1seconds)
     append!(vm, "vulta-main", s)
 end
 
