8bb21c60631855a99bbfcaaf724e963e2e6719a4
emmy <52672675+emmettgb@users.noreply.github.com>
2023-01-08 20:34:31 -0600

    field spawning; collision; joining

--- src/PositionVectors.jl
+++ src/PositionVectors.jl
@@ -13,11 +13,20 @@ end
 
 length(c::Component{:circle}) = parse(Int64, c["r"])
 
-length(c::Component{:rect}) = parse(Int64, c["height"])
+length(c::Component{<: Any}) = parse(Int64, c["height"])
 
 width(c::Component{:circle}) = parse(Int64, c["r"])
 
-width(c::Component{:rect}) = parse(Int64, c["width"])
+width(c::Component{<: Any}) = parse(Int64, c["width"])
+
+center(c::Component{<: Any}) = begin
+    p = position(c)
+    p.x += width(c) / 2
+    p.y += length(c) / 2
+    p
+end
+
+center(c::Component{:circle}) =  position(c)
 
 position(comp::Component{:circle}) = Vec2(parse(Int64, comp["cx"]),
         parse(Int64, comp["cy"]))
@@ -29,17 +38,19 @@ delta(c::Connection) = c[:VultaCore].deltas[find_client(c)]
 
 initializing(c::Connection) = delta(c) == 1
 
-function spawn!(vm::AbstractComponentModifier, s::Component{<: Any}, v::Vec2 = Vec2(0, 0);
-    decay::Int64 = 1)
+function spawn!(c::Connection, vm::AbstractComponentModifier, s::Component{<: Any},
+    v::Vec2 = Vec2(0, 0); decay::Int64 = 1)
+    c[:VultaCore].fields[replace(find_client(c), "rpc" => "")][s] = v
     s["x"] = v.x; s["y"] = v.y; s["fx"] = 0; s["fy"] = 0; s["decay"] = decay
-    style!(s, "transition" => 1seconds)
+    style!(s, "transition" => ".2s")
     append!(vm, "vulta-main", s)
 end
 
-function spawn!(vm::AbstractComponentModifier, s::Component{:circle}, v::Vec2 = Vec2(0, 0);
+function spawn!(c::Connection, vm::AbstractComponentModifier, s::Component{:circle}, v::Vec2 = Vec2(0, 0);
     decay::Int64 = 1)
+    c[:VultaCore].fields[replace(find_client(c), "rpc" => "")][s] = v
     s["cx"] = v.x; s["cy"] = v.y; s["fx"] = 0; s["fy"] = 0; s["decay"] = decay
-    style!(s, "transition" => 1seconds)
+    style!(s, "transition" => ".2s")
     append!(vm, "vulta-main", s)
 end
 
@@ -47,30 +58,35 @@ function is_colliding(vm::AbstractComponentModifier, s1::Component{<:Any},
     s2::Component{<:Any})
     s1 = vm.rootc[s1.name]
     s2 = vm.rootc[s2.name]
-    s1pos = position(s1); s2pos = position(s2)
-    diffx, diffy = abs(s1pos.x) - abs(s2pos.x), abs(s1pos.y) - abs(s2pos.y)
-    combined_width::Int64 = width(s1) + width(s2)
-    combined_length::Int64 = length(s1) + length(s2)
-    if abs(diffy) < combined_length - 2 && abs(diffx) < combined_width - 2
-        println(diffx, "  ", diffy)
-        println("::", combined_width, "  ", combined_length)
+    s1pos = center(s1); s2pos = center(s2)
+    diffx, diffy = abs(s1pos.x - s2pos.x), abs(s1pos.y - s2pos.y)
+    combined_width::Int64 = width(s1) / 2 + width(s2) / 2
+    combined_length::Int64 = length(s1) / 2 + length(s2) / 2
+    if abs(diffy) <= combined_length && abs(diffx) <= combined_width
         true
     else
         false
     end
 end
 
+above(v::Vec2, v2::Vec2) = begin
+
+end
+
 function translate!(cm::AbstractComponentModifier, comp::Component{:circle}, v::Vec2)
     cm[comp] = "cx" => v.x; cm[comp] = "cy" => v.y
 end
 
-function translate!(cm::AbstractComponentModifier, comp::Component{:rect}, v::Vec2)
+function translate!(cm::AbstractComponentModifier, comp::Component{<: Any}, v::Vec2)
     cm[comp] = "x" => v.x; cm[comp] = "y" => v.y
 end
 
-
-
 function force!(cm::AbstractComponentModifier, s::Component{<:Any}, force::Vec2)
+    cm[s] = "fx" => parse(Int64, cm[s]["fx"]) + force.x
+    cm[s] = "fy" => parse(Int64, cm[s]["fy"]) + force.y
+end
+
+function hard_force!(cm::AbstractComponentModifier, s::Component{<:Any}, force::Vec2)
     cm[s] = "fx" => force.x
     cm[s] = "fy" => force.y
 end
--- src/Vulta.jl
+++ src/Vulta.jl
@@ -10,23 +10,47 @@ mutable struct VultaCore <: Toolips.ServerExtension
     type::Symbol
     peers::Dict{String, Dict{String, Servable}}
     deltas::Dict{String, Int64}
+    fields::Dict{String, Dict{Servable, VultaVector}}
     function VultaCore()
-        new(:connection, Dict{String, Servable}(), Dict{String, Int64}())
+        new(:connection, Dict{String, Servable}(), Dict{String, Int64}(),
+        Dict{String, Dict{Servable, VultaVector}}())
     end
 end
 
 function initialize(f::Function, c::Connection, bod::Component{:body},
-    width::Int64 = 1280, height::Int64 = 720; tickrate::Int64 = 100)
+    width::Int64 = 1280, height::Int64 = 720; tickrate::Int64 = 50)
     push!(c[:VultaCore].peers, getip(c) * "rpc" => Dict{String, Servable}())
      push!(c[:VultaCore].deltas, getip(c) * "rpc" => 0)
+     push!(c[:VultaCore].fields, getip(c) => Dict{Servable, VultaVector}())
     open_rpc!(c, getip(c) * "rpc", tickrate = tickrate) do cm::ComponentModifier
         c[:VultaCore].deltas[getip(c) * "rpc"] += 1
+        [begin
+        component = cm[comp[1]]
+        vec = position(component)
+        c[:VultaCore].fields[getip(c)][comp[1]] = vec
+        end for comp in c[:VultaCore].fields[getip(c)]]
         [force(cm, comp) for comp in values(cm.rootc)]
         f(cm)
     end
     wind = svg("vulta-main", width = width, height = height)
     push!(bod, wind)
     style!(wind, "padding" => 0px, "position" => "absolute")
+    write!(c, bod)
+end
+
+function join(c::Connection, host::String,
+    bod::Component{:body}, width::Int64 = 1280,
+    height::Int64 = 720; tickrate::Int64 = 100)
+    wind = svg("vulta-main", width = width, height = height)
+    push!(bod, wind)
+    style!(wind, "padding" => 0px, "position" => "absolute")
+    first = true
+    join_rpc!(c, host, tickrate = tickrate) do cm::ComponentModifier
+        client_name = replace(find_client(c), "rpc" => "")
+        for comp in c[:VultaCore].fields[client_name]
+            spawn!(c, cm, comp[1], comp[2])
+        end
+    end
     write!(c, bod)
 end
 
@@ -46,7 +70,8 @@ function force(cm::ComponentModifier, comp::Component{:circle})
         else
             cm[comp] = "fx" => fx + (decay)
         end
-    elseif fy != 0
+    end
+    if fy != 0
         cm[comp] = "cy" => curry + fy
         if fy > 0
             cm[comp] = "fy" => (fy - decay)
@@ -91,6 +116,6 @@ end
 
 
 export spawn!, translate!, VultaModifier, Vec2, Vec3, force!, is_colliding, initialize
-export initializing, delta
+export initializing, delta, hard_force!
 export VultaCore
 end # - module
