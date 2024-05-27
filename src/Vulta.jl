cecf955e04f7bd69972f65750006a63ecc96cdfe
emmy <52672675+emmaccode@users.noreply.github.com>
2024-05-27 07:37:01 -0400

    back on vulta for `0.3`

--- src/Vulta.jl
+++ src/Vulta.jl
@@ -6,52 +6,16 @@ using ToolipsSVG
 
 include("PositionVectors.jl")
 
-mutable struct VultaCore <: Toolips.ServerExtension
-    type::Symbol
-    peers::Dict{String, Dict{String, Servable}}
-    deltas::Dict{String, Int64}
-    fields::Dict{String, Dict{Servable, VultaVector}}
-    function VultaCore()
-        new(:connection, Dict{String, Servable}(), Dict{String, Int64}(),
-        Dict{String, Dict{Servable, VultaVector}}())
-    end
-end
-
-function initialize(f::Function, c::Connection, bod::Component{:body},
-    width::Int64 = 1280, height::Int64 = 720; tickrate::Int64 = 50)
-    push!(c[:VultaCore].peers, getip(c) * "rpc" => Dict{String, Servable}())
-     push!(c[:VultaCore].deltas, getip(c) * "rpc" => 0)
-     push!(c[:VultaCore].fields, getip(c) => Dict{Servable, VultaVector}())
-    open_rpc!(c, getip(c) * "rpc", tickrate = tickrate) do cm::ComponentModifier
-        c[:VultaCore].deltas[getip(c) * "rpc"] += 1
-        [begin
-        component = cm[comp[1]]
-        vec = position(component)
-        c[:VultaCore].fields[getip(c)][comp[1]] = vec
-        end for comp in c[:VultaCore].fields[getip(c)]]
-        [force(cm, comp) for comp in values(cm.rootc)]
-        f(cm)
-    end
-    wind = svg("vulta-main", width = width, height = height)
-    push!(bod, wind)
-    style!(wind, "padding" => 0px, "position" => "absolute")
-    write!(c, bod)
-end
-
-function join(c::Connection, host::String,
-    bod::Component{:body}, width::Int64 = 1280,
-    height::Int64 = 720; tickrate::Int64 = 100)
-    wind = svg("vulta-main", width = width, height = height)
-    push!(bod, wind)
-    style!(wind, "padding" => 0px, "position" => "absolute")
-    first = true
-    join_rpc!(c, host, tickrate = tickrate) do cm::ComponentModifier
-        client_name = replace(find_client(c), "rpc" => "")
-        for comp in c[:VultaCore].fields[client_name]
-            spawn!(c, cm, comp[1], comp[2])
+mutable struct VultaCore{D <: VultaVector} <: Toolips.ServerExtension
+    pages::Dict{String, Vector{Pair{Component{<:Any}, D}}}
+    delta::Float64
+    function VultaCore(d::Symbol = :2D)
+        vec = Vec2
+        if d == :3D
+            vec = Vec3
         end
+        new{vec}(Dict{String, Vector{Pair{Component{<:Any}, D}}}())
     end
-    write!(c, bod)
 end
 
 function force(cm::ComponentModifier, comp::Component{:circle})
